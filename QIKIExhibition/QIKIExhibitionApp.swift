373b14d5edca8902f72eda430a6096b387493cea
Nirmit Dagly <nirmit@miamedia.com.au>
2025-05-05 11:04:45 +1000

    Initial Commit

---
+++ QIKIExhibition/QIKIExhibitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QIKIExhibitionApp.swift
+//  QIKIExhibition
+//
+//  Created by Miamedia on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct QIKIExhibitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
